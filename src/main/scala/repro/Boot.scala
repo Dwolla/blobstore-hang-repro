a2c31207494991a18b769163fe7bd39ad81bc921
Brian Holt <bholt@dwolla.com>
2022-06-24 14:01:35 -0500

    play around with the chunk sizes

--- src/main/scala/repro/Boot.scala
+++ src/main/scala/repro/Boot.scala
@@ -3,21 +3,33 @@ package repro
 import cats.effect._
 import cats.effect.std.Console
 import cats.syntax.all._
+import cats.effect.syntax.all._
 import fs2._
 import fs2.io.{readOutputStream, writeOutputStream}
 
 object Boot extends IOApp.Simple {
   private def stream[F[_] : Console : Async](size: Long): Stream[F, Nothing] =
     Stream
-      .repeatEval(Array.fill(10.megabytes)(Byte.MinValue).pure[F])
-      .map(Chunk.array(_))
+      .emit(Byte.MinValue)
+      .repeat
+      .chunkN(4.megabytes, allowFewer = false)
       .zipWithIndex
       .evalMap { case (c, idx) =>
         Console[F].println(s"👀 processing chunk #$idx of ${c.size} bytes")
           .as(c)
       }
       .flatMap(Stream.chunk)
-      .take(size)
+
+      /*
+       * Source stream chunk size | readOutputStream Chunk Size | Take at least…
+       * -------------------------|-----------------------------|--------------
+       *             1            |               1             |       4
+       *             1            |               2             |       5
+       *             1            |               3             |       7
+       *            4MB           |              1MB            |      3MB
+       */
+
+      .take(3.megabytes)
       .through { in =>
         // This is a simplification of what fs2-pgp does to pipe the stream through BouncyCastle's various OutputStreams
         readOutputStream(1.megabyte) { out =>
@@ -25,14 +37,14 @@ object Boot extends IOApp.Simple {
             .through(writeOutputStream(out.pure[F], closeAfterUse = true))
             .compile
             .drain
-            .flatTap(_ => Console[F].println("✍️ writeOutputStream complete"))
+            .guarantee(Console[F].println("✍️ writeOutputStream complete"))
         }
       }
       .through {
         _.drop(1) >> // without this drop (or maybe just something that reads from the input stream), the hang condition doesn't seem to be triggered
           Stream.raiseError(new RuntimeException("boom"))
-      }
-      .drain ++ Stream.eval(Console[F].println("😇 completed stream")).drain
+      } ++
+      Stream.eval(Console[F].println("😇 completed stream")).drain
 
   private def runF[F[_] : Async : Console]: Stream[F, Unit] =
     stream(1.gigabyte) ++ Stream.emit(())
